0c2fa5fc6fc3b4bb33f4cc2a0d120ddd952b35bb
StephanyKleen <stephanykleen@gmail.com>
2023-08-01 20:44:06 -0700

    Passing all tests of ttt.js

--- starter/class/ttt.js
+++ starter/class/ttt.js
@@ -25,17 +25,79 @@ class TTT {
 
   // Remove this
   static testCommand() {
+    
     console.log("TEST COMMAND");
+
+
   }
-
   static checkWin(grid) {
-
     // Return 'X' if player X wins
     // Return 'O' if player O wins
     // Return 'T' if the game is a tie
     // Return false if the game has not ended
 
-  }
+    //empty grid
+    let empty = true;
+    grid.forEach(row => row.forEach(cell =>{ 
+      if(cell !== " " ){
+        return empty = false} 
+      }));
+        if (empty) {
+          return false;
+        }
+
+    //horizontal
+    let horizontalWinner;
+    grid.forEach(
+      row => {
+        if (row[0] !== " " && row[0] === row[1] && row[0] === row[2]) {
+          horizontalWinner = row[0];
+        }
+      }
+    );
+    if (horizontalWinner) {
+      return horizontalWinner;
+    }
+
+    //vertical
+    let verticalWinner;
+    for (let i = 0; i < 3; i++) {
+      if (grid[0][i] !== " " && grid[0][i] === grid[1][i] && grid[0][i] === grid[2][i]) {
+        verticalWinner = grid[0][i];
+      }
+    }
+    if (verticalWinner) {
+      return verticalWinner;
+    }
+
+    //diagonal
+    let diagonalWinner;
+
+    if (grid[0][0] !== " " && grid[0][0] === grid[1][1] && grid[0][0] === grid[2][2]) {
+      diagonalWinner = grid[0][0];
+    }
+
+    if (grid[2][0] !== " " && grid[2][0] === grid[1][1] && grid[2][0] === grid[0][2]) {
+      diagonalWinner = grid[2][0];
+    }
+
+    if (diagonalWinner) {return diagonalWinner}
+
+    //tie
+    let tie = "T";
+    grid.forEach(
+      row => {
+        row.forEach(
+          cell => {
+            if (cell === " ") {tie = false}
+          }
+        );
+      }
+    );
+    return tie;
+
+    }
+  
 
   static endGame(winner) {
     if (winner === 'O' || winner === 'X') {
